e10a2da988447bff9168571a831b34dc74121c82
Ted Ralphs <ted@lehigh.edu>
2020-09-07 16:47:34 -0400

    Revert "Removing export to see if it makes a difference"
    This reverts commit da5ce050c5732e450c1d48752f73a4744506df5b.

--- .ci/setup_environment.sh
+++ .ci/setup_environment.sh
@@ -3,15 +3,15 @@
 case $CC in
     gcc*)
         if [ $TRAVIS_OS_NAME = osx ]; then
-            CC=gcc-9
-            CXX=g++-9
-            CCVERSION=gcc9
+            export CC=gcc-9
+            export CXX=g++-9
+            export CCVERSION=gcc9
         else
-            CCVERSION=gcc$($CC -dumpversion)
+            export CCVERSION=gcc$($CC -dumpversion)
         fi
         ;;
     clang)
-        CCVERSION=clang$(clang --version | fgrep version | \
+        export CCVERSION=clang$(clang --version | fgrep version | \
                                 sed "s/.*version \([0-9]*\.[0-9]*\).*/\1/" | \
                                 cut -d "." -f 1)
         ;;
@@ -19,32 +19,32 @@ esac
 declare -a DBG_ARGS
 declare -a ADD_ARGS
 declare -a COMMON_ARGS
-DBG_ARGS=()
-ADD_ARGS=()
-COMMON_ARGS=()
+export DBG_ARGS=()
+export ADD_ARGS=()
+export COMMON_ARGS=()
 if [ "$DEBUG" = "true" ]; then
-    DBGN="-dbg"
-    DBG_ARGS+=( --enable-debug )
-    CXXFLAGS="-Og -g"
+    export DBGN="-dbg"
+    export DBG_ARGS+=( --enable-debug )
+    export CXXFLAGS="-Og -g"
 fi
 if [ "$ASAN" = "true" ]; then
-    ASN="-asan"
-    ADD_CXXFLAGS="${ADD_CXXFLAGS} -fsanitize=address"
-    LDFLAGS="-lasan"
+    export ASN="-asan"
+    export ADD_CXXFLAGS="${ADD_CXXFLAGS} -fsanitize=address"
+    export LDFLAGS="-lasan"
 fi
 if [ "$BUILD_STATIC" = "true" ]; then
-    STATIC="-static"
-    ADD_ARGS+=( --static --with-lapack='-llapack -lblas -lgfortran -lquadmath -lm' )
+    export STATIC="-static"
+    export ADD_ARGS+=( --static --with-lapack='-llapack -lblas -lgfortran -lquadmath -lm' )
 fi
 if [ "$CXX_FLAGS" != "" ]; then
-    ADD_ARGS+=( CXXFLAGS=${CXXFLAGS} )
+    export ADD_ARGS+=( CXXFLAGS=${CXXFLAGS} )
 fi
 if [ "$LD_FLAGS" != "" ]; then
-    ADD_ARGS+=( LDFLAGS=${LDFLAGS} )
+    export ADD_ARGS+=( LDFLAGS=${LDFLAGS} )
 fi
-COMMON_ARGS=( --no-prompt --verbosity ${VERBOSITY:-2} --tests main --enable-relocatable )
-PLATFORM=$TRAVIS_OS_NAME${OSX:-}-x86_64-$CCVERSION
-PROJECT_URL=https://github.com/$TRAVIS_REPO_SLUG
+export COMMON_ARGS=( --no-prompt --verbosity ${VERBOSITY:-2} --tests main --enable-relocatable )
+export PLATFORM=$TRAVIS_OS_NAME${OSX:-}-x86_64-$CCVERSION
+export PROJECT_URL=https://github.com/$TRAVIS_REPO_SLUG
 if [ $TRAVIS_OS_NAME = windows ]; then
-    PATH=/C/tools/msys64/mingw64/bin:$PATH
+    export PATH=/C/tools/msys64/mingw64/bin:$PATH
 fi
